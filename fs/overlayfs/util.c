31328b9296e81f3cb616bb40111bb33690946f75
anVzdGFtb25 <77651965+anVzdGFtb25@users.noreply.github.com>
2024-07-30 21:18:01 +0800

    overlayfs:dont make DCACHE_OP_{HASH,COMPARE} weird

--- fs/overlayfs/util.c
+++ fs/overlayfs/util.c
@@ -99,8 +99,6 @@ bool ovl_dentry_remote(struct dentry *dentry)
 bool ovl_dentry_weird(struct dentry *dentry)
 {
 	return dentry->d_flags & (DCACHE_NEED_AUTOMOUNT |
-				  DCACHE_MANAGE_TRANSIT |
-				  DCACHE_OP_HASH |
 				  DCACHE_OP_COMPARE);
 }
 
